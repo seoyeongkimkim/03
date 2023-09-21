3b06ffc5287b17d89b8ac367a5bb8f3fde0da1ae
seoyeongkimkim <144990861+seoyeongkimkim@users.noreply.github.com>
2023-09-21 16:10:51 +0900

    2

--- main.c
+++ main.c
@@ -5,16 +5,15 @@
 
 int main(int argc, char *argv[]) 
 {
-	int input_int;
-	float input_float;
+	char c;
+	int i;
 	
-	printf("enter an integer : ");
-	scanf("%d", &input_int);
+	printf("input a number : ");
+	scanf("%c", &c);
 	
-	printf("enter a float : "); 
-	scanf("%f", &input_float);
-	
-	printf("integer : %d, float : %f\n", input_int, input_float);
+	i = c - '0';
+	printf("The input number ASCII is %i\n", c);
+	printf("The input number is %i\n", i);
 	
 	system("PAUSE");
 	return 0;
